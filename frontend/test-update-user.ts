d233461343ae8b8a2591c480a15b22268e99264e
KumaHo11 <javi.osorio.1@gmail.com>
2026-06-15 22:21:36 -0300

    fix: remove test scripts that break typescript build

--- frontend/test-update-user.ts
+++
@@ -1,7 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
-const credential = admin.credential.cert(JSON.parse(Buffer.from(process.env.FIREBASE_ADMIN_CREDENTIALS_BASE64 || '', 'base64').toString('utf-8')));
-admin.initializeApp({ credential });
-admin.auth().updateUser('random-uid', { emailVerified: true }).catch(e => console.error(e.code));
